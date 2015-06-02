87321b99ff4d1c21dbf93f5865467a92ce30f9d2
Julian <yoda@unknowns.fr>
2015-06-02 15:31:29 +0200

    encding

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
-Rails.application.initialize!
-
 Encoding.default_internal = Encoding::UTF_8
 Encoding.default_external = Encoding::UTF_8
+
+# Initialize the Rails application.
+Rails.application.initialize!
