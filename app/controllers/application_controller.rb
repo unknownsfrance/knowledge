7298179d69699b4ec0313162d1e26f22320f998f
Julian <yoda@unknowns.fr>
2015-06-02 15:08:27 +0200

    encding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_charsets
+  #before_filter :configure_charsets
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def configure_charsets
-    response.headers["Content-Type"] = "text/html; charset=utf-8"
-    suppress(ActiveRecord::StatementInvalid) do 
-      ActiveRecord::Base.connection.execute 'SET NAMES UTF8'
-    end
-  end
+  #def configure_charsets
+  #  response.headers["Content-Type"] = "text/html; charset=utf-8"
+  #  suppress(ActiveRecord::StatementInvalid) do 
+  #    ActiveRecord::Base.connection.execute 'SET NAMES UTF8'
+  #  end
+  #end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Encoding.default_internal = Encoding::UTF-8
+Encoding.default_external = Encoding::UTF-8
