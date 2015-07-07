464a8a9c4ff25aef4776720c7d0ad215083543be
Julian <yoda@unknowns.fr>
2015-07-07 17:00:21 +0200

    ebextensions

--- .ebextensions/setup.sh
+++ .ebextensions/setup.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-chmod 777 -R /solr /tmp /log
-RAILS_ENV=production rake sunspot:solr:stop
-RAILS_ENV=production rake sunspot:solr:start
-RAILS_ENV=production rake sunspot:reindex
+#chmod 777 -R /solr /tmp /log
+#RAILS_ENV=production rake sunspot:solr:stop
+#RAILS_ENV=production rake sunspot:solr:start
+#RAILS_ENV=production rake sunspot:reindex
+
+. /opt/elasticbeanstalk/support/envvars
+cd $EB_CONFIG_APP_CURRENT
+su -c "chmod 777 -R /solr /tmp /log"
+su -c "RAILS_ENV=production rake sunspot:reindex" $EB_CONFIG_APP_USER
